1e5cc1d38c5088a0e98765063ea2f1d44fb16a06
ACenTe25 <alex.cendejas@gmail.com>
2023-01-05 09:14:37 -0600

    apertura de twinkle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::process::{Command, Output};
+use std::io::Read;
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    
+    println!("[I] ARRANQUE");
+
+    abrir_twinkle();
+
+}
+
+fn abrir_twinkle() {
+
+    println!(" [I] ABRIENDO TWINKLE...");
+
+    let mut proc_twinkle = Command::new("twinkle")
+    .args(["-c", "-f", "twinkle.cfg"])
+    .spawn()
+    .expect("[E] FALLA AL ABRIR TWINKLE");
+
+    sleep(Duration::from_secs(10));
+
+    let mut stdout_twinkle = String::new();
+
+    proc_twinkle.stdout.take().unwrap().read_to_string(&mut stdout_twinkle)
+    .expect("[E] FALLA AL LEER STDOUT DE ABRIR TWINKLE");
+
+    let mut stderr_twinkle = String::new();
+
+    proc_twinkle.stderr.take().unwrap().read_to_string(&mut stderr_twinkle)
+    .expect("[E] FALLA AL LEER STDERR DE ABRIR TWINKLE");
+
+    Command::new("twinkle")
+    .args(["-c", "--cmd", "quit"])
+    .spawn()
+    .expect("[E] FALLA AL TRATAR DE CERRAR TWINKLE")
+    .wait()
+    .expect("[E] FALLA AL TRATAR DE CERRAR TWINKLE");
+
+    println!("  [I] STDOUT ABRIR TWINKLE: {}", stdout_twinkle);
+
+    println!("  [I] STDERR ABRIR TWINKLE: {}", stderr_twinkle);
+
 }
