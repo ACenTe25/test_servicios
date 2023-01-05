c73c4bc24ac5579ecaad6c928fd1615cdb2cfe48
ACenTe25 <alex.cendejas@gmail.com>
2023-01-05 10:05:33 -0600

    Y con ls?

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     
     println!("[I] ARRANQUE");
 
-    abrir_twinkle_con_sh();
+    hacer_ls_con_sh();
 
     println!("[I] FIN");
 
@@ -105,4 +105,41 @@ fn abrir_twinkle_con_sh() {
     println!("  [I] STDOUT ABRIR TWINKLE: {}", stdout_twinkle);
 
     println!("  [I] STDERR ABRIR TWINKLE: {}", stderr_twinkle);
+}
+
+fn hacer_ls_con_sh() {
+    println!(" [I] HACIENDO ls -alh CON sh...");
+
+    let mut proc_twinkle = Command::new("sh")
+    .args(["-c", "ls -alh"])
+    .spawn()
+    .expect("[E] FALLA AL HACER ls");
+
+    sleep(Duration::from_secs(10));
+
+    let mut stdout_twinkle = String::new();
+
+    let stdout_child = match proc_twinkle.stdout.take() {
+        Some(mut stdout) => stdout.read_to_string(&mut stdout_twinkle).expect("[E] FALLA AL LEER STDOUT DE sh"),
+        None => {
+            stdout_twinkle = "(stdout vacío)".to_string();
+            0
+        }
+    };
+
+    let mut stderr_twinkle = String::new();
+
+    let stderr_child = match proc_twinkle.stderr.take() {
+        Some(mut stderr) => stderr.read_to_string(&mut stderr_twinkle).expect("[E] FALLA AL LEER STDERR DE sh"),
+        None => {
+            stderr_twinkle = "(stderr vacío)".to_string();
+            0
+        }
+    };
+
+    proc_twinkle.wait();
+
+    println!("  [I] STDOUT ls: {}", stdout_twinkle);
+
+    println!("  [I] STDERR ls: {}", stderr_twinkle);
 }
